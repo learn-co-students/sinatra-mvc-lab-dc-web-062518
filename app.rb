03c0ecdc8d1f87838dfacbf59210a92f6c4a25aa
github username <github email address>
2018-07-17 15:44:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @phrase = params[:user_phrase]
+    @piglatinized_phrase = PigLatinizer.new.piglatinize(@phrase)
+    erb :piglatinize
+  end
+
 end
