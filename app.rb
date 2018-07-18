9e3fcb1c7aca81ed83fc19f98c54647dceccf8ad
JohnDaise <jdaise@gmail.com>
2018-07-18 17:49:56 -0400

    first commit

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+
+
+    get '/user_input' do
+
+        erb :user_input
+    end
+
+
+    get '/' do
+      erb :user_input
+    end
+
+
+    post '/piglatinize' do
+      @result = PigLatinizer.new(params[:user_phrase])
+      # binding.pry
+        erb :piglatinize
+    end
+
+
+
+
+
+
 end
